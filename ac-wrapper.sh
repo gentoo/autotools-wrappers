gentoo/autotools-wrappers

commit 6f9e65b68174b938714100f34f399844f8d2cb93

autoconf-wrapper: fix version detection
Make sure we chop the trailing period from the version string to fix
comparing to installed versions.  Otherwise, "2.69." will never match
"2.69".

Signed-off-by: Mike Frysinger <vapier@gentoo.org>

ac-wrapper.sh
@@ -146,7 +146,7 @@ acprereq_version() {
 }
 
 generated_version() {
-	local re='^# Generated (by (GNU )?Autoconf|automatically using autoconf version) ([0-9.]+).*'
+	local re='^# Generated (by (GNU )?Autoconf|automatically using autoconf version) ([0-9.]+)\..*'
 	sed -n -E "/${re}/{s:${re}:\3:;p;q}" "$@"
 }
 

commit afe80def609c91d72df73837ce2c51fed8b69f28

ac-wrapper.sh: fix autoconf regex
We need to optionally match the ., not require digits following period in version group.

Bug: https://bugs.gentoo.org/870169
Fixes: 6f9e65b68174b938714100f34f399844f8d2cb93
Signed-off-by: Arsen Arsenović <arsen@aarsen.me>
Signed-off-by: Sam James <sam@gentoo.org>

ac-wrapper.sh
@@ -147,7 +147,7 @@ acprereq_version() {
 }
 
 generated_version() {
-	local re='^# Generated (by (GNU )?Autoconf|automatically using autoconf version) ([0-9.]+)\..*'
+	local re='^# Generated (by (GNU )?Autoconf|automatically using autoconf version) (([0-9]+.)+[0-9]+).*$'
 	sed -n -E "/${re}/{s:${re}:\3:;p;q}" "$@"
 }
 